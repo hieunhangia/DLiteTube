28af74fcc1e3eaaa89565414d984bdfe523b8920
hieunhangia <hieuvippro531@gmail.com>
2026-03-12 02:17:34 +0700

    initialize DLiteTube project files

---
+++ DLiteTube/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace DLiteTube.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ DLiteTube/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DLiteTube.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
